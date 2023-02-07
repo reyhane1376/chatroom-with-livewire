cd1c72a8e03871420026725d279476db11c8a2ed
reyhane <97376622+reyhane1376@users.noreply.github.com>
2023-02-07 08:58:30 +0330

    add-html-in-page-room

--- resources/views/livewire/room/index.blade.php
+++ resources/views/livewire/room/index.blade.php
@@ -1,3 +1,20 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    {{-- The whole world belongs to you --}}
+    <div class="bg-white shadow rounded-lg mt-10">
+        <div class="border-b border-gray-200 p-6">
+            <h2 class="text-2xl font-bold">Public Chat Rooms</h2>
+        </div>
+        <div class="p-6">
+
+        </div>
+        <div class="p-6">
+            <h3 class="text-xl font-bold pb-4 border-b mb-4">List Chat Room</h3>
+
+                <div>
+                    <a href="" class="text-lg font-medium hover:underline"></a>
+                </div>
+
+        </div>
+    </div>
+
 </div>
